10a1fab9e202ad487d07bdc4fd146b5fc900b70e
Arthur De Meyer <arthur.demeyer75@gmail.com>
2022-01-24 18:40:12 +0100

    new push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def download
+    send_file(
+      "#{Rails.root}/public/2018S14RapportActiviteės.pdf",
+      filename: "2018S14RapportActiviteės.pdf",
+      type: "application/pdf"
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get "home/download"
+  get 'connaitre', to: 'pages#connaitre', as: 'connaitre'
+  get 'soutenir', to: 'pages#soutenir', as: 'soutenir'
+  get 'contact', to: 'pages#contact', as: 'contact'
+  get 'impact', to: 'pages#impact', as: 'impact'
+  get 'test', to: 'pages#test', as: 'test'
+
+
+  match 'download', to: 'pages#download', as: 'download', via: :get
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
