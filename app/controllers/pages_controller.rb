d993f3b68cb810e1ebddae6437fb8f80c62d79d8
Arthur De Meyer <arthur.demeyer75@gmail.com>
2022-02-25 16:48:11 +0100

    correction

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,4 +9,20 @@ class PagesController < ApplicationController
       type: "application/pdf"
     )
   end
+
+  def download2019
+    send_file(
+      "#{Rails.root}/public/2019S14RapportActiviteės.pdf",
+      filename: "2019S14RapportActiviteės.pdf",
+      type: "application/pdf"
+    )
+  end
+
+  def download2020
+    send_file(
+      "#{Rails.root}/public/2019S14RapportActiviteės.pdf",
+      filename: "2020S14RapportActiviteés.pdf",
+      type: "application/pdf"
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get "home/download"
+  # get "home/download2017"
+  get "home/download2019"
+  get "home/download2020"
+
   get 'connaitre', to: 'pages#connaitre', as: 'connaitre'
   get 'soutenir', to: 'pages#soutenir', as: 'soutenir'
   get 'contact', to: 'pages#contact', as: 'contact'
@@ -13,6 +17,10 @@ Rails.application.routes.draw do
 
 
   match 'download', to: 'pages#download', as: 'download', via: :get
+  match 'download2019', to: 'pages#download2019', as: 'download2019', via: :get
+  match 'download2020', to: 'pages#download2020', as: 'download2020', via: :get
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
